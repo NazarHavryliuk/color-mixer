563af13d196482b8cb5e7514199979124518948c
Nazar <nazar123321nazargr@gamil.com>
2026-02-18 01:28:45 +0200

    fix: fix bug

--- vitest.config.js
+++ vitest.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [
-      '**/e2e/**', 
-      '**/*.spec.e2e.js'
-    ]
+    include: ['__tests__/unit/**/*.test.js'], // тільки твої юніт-тести
+    exclude: ['node_modules/**', '**/e2e/**', '**/*.spec.e2e.js'] // виключаємо node_modules повністю
   }
 });
