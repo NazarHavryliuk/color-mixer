900bb3a0fc628763b6b8648c0771f34bc791c510
Nazar <nazar123321nazargr@gamil.com>
2026-04-15 23:15:39 +0300

    fix bug feature flags

--- main.js
+++ main.js
@@ -16,8 +16,7 @@ Sentry.init({
 });
 
 posthog.init("phc_CEaawL384zYxVKk39r8Cqe8RWaRqWRWRKnF6ajyLS5F3", {  
-  api_host: "https://app.posthog.com",  
-  ui_host: "https://eu.i.posthog.com",  
+  api_host: "https://eu.posthog.com",   
   person_profiles: 'always',  
   disable_session_recording: false, // явно вмикаємо (або просто не вказуй — буде увімкнено)  
   session_recording: {  
